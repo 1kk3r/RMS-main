bdb31c7d5f787de398a1f1ea66afae5e4c45a3bf
1kk3r <ikerfaridc@gmail.com>
2024-08-30 16:14:39 -0400

    Update
    30-08-2024

---
+++ frontend/src/app/components/FormTask.jsx
@@ -0,0 +1,64 @@
+"use client"
+import React from 'react'
+import { useState } from 'react'
+
+function FormTask() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(title, description);
+        const res = await fetch('${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks', {
+            method: "POST",
+            body: JSON.stringify({title, description}),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        const data = await res.json()
+        console.log(data)
+    };
+
+  return (
+    <div className="bg-slate-200 p-7">
+        <form onSubmit={handleSubmit}>
+            <h1 className="text-black font-bold">Añadir Tarea</h1>
+            <label
+            htmlFor="title"
+            className="text-xs text-black"
+            >
+            Titulo:
+            </label>
+            <input
+            type="text"
+            name="title"
+            className="bg-slate-400 rounded-md p-2 mb-2 block w-full text-slate-900"
+            onChange={e => setTitle(e.target.value)}
+            />
+
+            <label
+            htmlFor="title"
+            className="text-xs text-black"
+            >
+            Descripcion:
+            </label>
+            <textarea
+            name="description"
+            className="bg-slate-400 rounded-md p-2 mb-2 block w-full"
+            onChange={e => setDescription(e.target.value)}
+            />
+
+            <button
+            className="bg-indigo-500 text-white rounded-md p-2 block w-full"
+            type="submit"
+            name="save"
+            >
+                Save
+            </button>
+        </form>
+    </div>
+  )
+};
+
+export default FormTask
---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+import FormTask from "./components/FormTask"
+import ListTask from "./components/ListTask"
+
+function HomePage() {
+  return (
+    <div className="container mx-auto">
+      <h1>Tasks App</h1>
+      <div className="flex gap-x-10">
+      <FormTask />
+      <ListTask />
+      </div>
+    </div>
+  )
+}
+export default HomePage
