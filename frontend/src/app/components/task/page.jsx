1f8ff398fcd0468026d33fd51ead854af1eaab89
Iker <132531296+1kk3r@users.noreply.github.com>
2024-10-30 23:42:55 -0300

    Update page.jsx

--- frontend/src/app/components/task/page.jsx
+++ frontend/src/app/components/task/page.jsx
@@ -106,7 +106,7 @@ export default function TaskManager() {
         <div className="flex mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8 gap-5">
             <div className="bg-slate-200 p-7 mb-4 rounded-lg">
                 <form onSubmit={handleSubmit}>
-                    <h1 className="text-black font-bold text-2xl mb-4">Añadir Tarea</h1>
+                    <h1 className="text-black font-bold text-2xl mb-4">Add Task</h1>
 
                     {error && <p className="text-red-500 mb-4">{error}</p>}
 
@@ -147,7 +147,7 @@ export default function TaskManager() {
 
             <div className="bg-slate-700 p-4 w-full rounded-lg">
                 <h1 className="text-white text-2xl font-bold mb-4">
-                    Lista de Tareas
+                    Task List
                 </h1>
                 {Array.isArray(tasks) ? (
                     tasks?.map((task) => (
