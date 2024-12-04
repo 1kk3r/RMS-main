16ee4d37ae41eb2f52a5691c9295627736fcc828
Iker <ikerfaridc@gmail.com>
2024-12-04 02:44:25 -0300

    Delete BackEnd

--- backend/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import TaskViewSet
-
-router = routers.DefaultRouter()
-
-router.register('api/tasks', TaskViewSet, 'tasks')
-
-urlpatterns = router.urls
